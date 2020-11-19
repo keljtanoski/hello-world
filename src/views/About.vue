5baf02ccadb29f0ebd4cbf9e0e26fd966c875ec4
Kostadin Keljtanoski <keljtanoski@gmail.com>
2020-11-19 22:52:31 +0100

    Updating node, adding tailwind, postcss

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <CV />
-  </div>
+  <!--        ########### ABOUT ME SECTION START ##################-->
+  <section id="about" class="about bg__javascript2">
+    <h2 class="heading">
+      <i class="fa fa-address-card fa-fw custom-title-icon"></i>
+      About me
+    </h2>
+    <div class="row">
+      <div class="col-md-12 section-content">
+        <p class="">
+          Certified Laravel Developer | Project Lead Developer | Senior Software Engineer | Senior
+          Full Stack PHP Developer
+          with 6&#43; years of
+          professional experience, profound in all of the phases of Software Development Life Cycle (SDLC)
+          for designing and building complex web applications and APIs. Perform business and technology
+          analysis, system integration engineering, custom software development, support and maintenance,
+          refactoring and re-engineering, unit testing and validation of services and solutions.
+          Experienced in agile development and design of high volume and high quality software components
+          and applications. Currently working with the Laravel Framework. Always thrilled to learn new
+          stuff.
+        </p>
+      </div>
+    </div>
+  </section>
+  <!--        ########### ABOUT ME SECTION END ##################-->
 </template>
 <script>
 // @ is an alias to /src
-import CV from '@/components/CV.vue'
-export default {
-  name: 'About',
-  components: {
-    CV
-  }
-}
+// import CV from '@/components/CV.vue'
+//
+// export default {
+//   name: 'About',
+//   components: {
+//     CV
+//   }
+// }
 
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+// module.exports = {
+//   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+let colors = require('tailwindcss/colors');
+
+module.exports = {
+    theme: {
+        inset: {
+            '1/2': '50%',
+            '-5': '-5px',
+            '-10': '-10px',
+            '-15': '-15px',
+            '-55': '-55%;',
+            '-8': '-32px',
+            114: '114px',
+            96: '96px',
+            0: 0,
+            90: '90px',
+            80: '80px',
+        },
+        extend: {
+            margin: {
+                none: 'initial',
+                26: '7rem',
+            },
+            maxWidth: {
+                card: '410px',
+            },
+            minWidth: {
+                card: '344px',
+            },
+            height: {
+                card: '200px',
+                priceContent: '234px',
+            },
+            width: {
+                pricing: '335px',
+                245: '245px',
+                280: '280px',
+                345: '345px',
+                hr: '200px',
+            },
+            colors,
+            fontFamily: {
+                sans: ['proxima-soft', 'sans-serif'],
+            },
+        },
+    },
+    variants: {
+        textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        textDecoration: ['group-hover'],
+        minWidth: ['responsive'],
+        opacity: ['group-hover'],
+        backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        backgroundOpacity: ['responsive', 'hover', 'focus'],
+    },
+    plugins: [
+        // ...
+    ],
+};
