2fd2bc59ce3c96534cc32a439f9329d44cdd7f1d
Kostadin Keljtanoski <keljtanoski@gmail.com>
2020-11-19 19:39:32 +0100

    Adding Vue router

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <CV />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import CV from '@/components/CV.vue'
+export default {
+  name: 'About',
+  components: {
+    CV
+  }
+}
+
+</script>
