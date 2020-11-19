b7108f097ac9b87798c1b9722bd27e5a57a23a05
Kostadin Keljtanoski <keljtanoski@gmail.com>
2020-11-19 19:52:24 +0100

    Adding another route and view component

---
+++ src/views/Resume.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="resume">
+    <h1>This is a resume page</h1>
+    <CV />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import CV from '@/components/CV.vue'
+export default {
+  name: 'Resume',
+  components: {
+    CV
+  }
+}
+
+</script>
